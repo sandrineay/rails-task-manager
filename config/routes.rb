3c39fe429444381d4a12136c02697041d2f9bca3
Sandrine Ayral <sandrine.ayral@gmail.com>
2017-05-23 12:11:12 +0100

    added 7 routes and appropriate controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # CRUD ROUTES
+
+  # read all the task
+  get 'tasks', to: 'tasks#index'
+  # read one task
+  get 'tasks/:id', to: 'tasks#show'
+
+  # add a new task to the app: retrieve info from form and feed it to the index
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
+  # update the info of a given task
+  get 'tasks/:id/edit', to: 'tasks#edit'
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # delete a task
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
